afe64f35bbc9988a0e659b4fa4905c06affe3b9b
Алексей Кузьмин <akuzmin340@gmail.com>
2025-02-05 20:20:52 +0200

    test login

--- domain/service/auth/auth_test.go
+++ domain/service/auth/auth_test.go
@@ -2,11 +2,15 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/mock"
 	"messanger/config"
+	cache "messanger/data/cache/local"
+	sms "messanger/data/sms/sms_chan"
 	"messanger/domain/models"
+	"messanger/domain/ports"
 	"messanger/domain/ports/mocks"
-	mocks2 "messanger/domain/service/auth/mocks"
+	"messanger/domain/service/phone"
 	"testing"
 )
 
@@ -16,6 +20,16 @@ var authCfg = &config.AuthServiceConfig{
 	AccessTokenSignKey:   "test-key",
 	DurationBlockUserMin: 5,
 	LoginAttempts:        1,
+}
+
+var c ports.Cache
+var smsChan *sms.SmsChan
+var phoneConf PhoneConfirmator
+
+func init() {
+	c = cache.NewCache()
+	smsChan = sms.NewSmsChan()
+	phoneConf = phone.NewPhoneService(smsChan, c)
 }
 
 func TestAuth(t *testing.T) {
@@ -30,25 +44,24 @@ func TestAuth(t *testing.T) {
 		Confirmed: true,
 	}
 
-	phoneCode := "12345"
-
-	cache := mocks.NewCache(t)
 	userRepo := mocks.NewUsersRepo(t)
-	phoneConf := mocks2.NewPhoneConfirmator(t)
-
-	cache.On("Get", mock.Anything, user.Phone).Return(0, nil)
-	cache.On("Del", context.Background(), user.Phone).Return(nil)
 
 	userRepo.On("GetByPhoneWithPass", mock.Anything, user.Phone, user.Password).Return(user, nil)
 	userRepo.On("GetById", mock.Anything, user.Id).Return(user, nil)
 
-	phoneConf.On("ToConfirming", mock.Anything, user.Id, user.Phone).Return(nil)
-	phoneConf.On("ConfirmUser", mock.Anything, phoneCode).Return(user.Id, nil)
-
-	auth := NewAuthService(cache, userRepo, phoneConf, authCfg)
+	auth := NewAuthService(c, userRepo, phoneConf, authCfg)
 
 	if err := auth.Login1FA(context.Background(), user.Phone, user.Password); err != nil {
 		t.Error(err)
 	}
+
+	code := <-smsChan.Chan
+	fmt.Println(user.Phone, code)
+	tokens, err := auth.Login2FA(context.Background(), user.Phone, code)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(tokens)
 
 }
