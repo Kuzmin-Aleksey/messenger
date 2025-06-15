efef9408c545b8fda6a97438499d08fd443c883c
Алексей Кузьмин <akuzmin340@gmail.com>
2025-06-15 23:12:18 +0200

    remove unused line

--- domain/service/auth/auth.go
+++ domain/service/auth/auth.go
@@ -168,7 +168,7 @@ func (s *AuthService) DecodeAccessToken(access string) (int, *errors.Error) {
 
 		return s.accessTokenKey, nil
 	})
-	_ = "transaction"
+
 	if err != nil {
 		return 0, errors.New(err, ErrInvalidToken, http.StatusUnauthorized)
 	}
